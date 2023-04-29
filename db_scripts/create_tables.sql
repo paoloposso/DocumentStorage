843ad3573867c899bce24a3a430862429556678b
Paolo Posso <pvictorsys@gmail.com>
2023-04-29 12:41:30 -0300

    Create procs

---
+++ db_scripts/create_tables.sql
@@ -0,0 +1,44 @@
+CREATE TABLE IF NOT EXISTS users (
+  id SERIAL PRIMARY KEY,
+  email VARCHAR(255) UNIQUE NOT NULL,
+  name VARCHAR(50) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  role VARCHAR(10) NOT NULL DEFAULT 'user',
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS groups (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(50) UNIQUE NOT NULL,
+  description TEXT,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS documents (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  description TEXT,
+  file_path TEXT NOT NULL,
+  created_by INTEGER REFERENCES users(id) NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS group_members (
+  group_id INTEGER REFERENCES groups(id) NOT NULL,
+  user_id INTEGER REFERENCES users(id) NOT NULL,
+  PRIMARY KEY (group_id, user_id)
+);
+
+CREATE TABLE IF NOT EXISTS document_access (
+  document_id INTEGER REFERENCES documents(id) NOT NULL,
+  group_id INTEGER REFERENCES groups(id),
+  user_id INTEGER REFERENCES users(id),
+  PRIMARY KEY (document_id, group_id, user_id)
+);
+
+CREATE INDEX IF NOT EXISTS users_email_idx ON users (email);
+
+CREATE INDEX IF NOT EXISTS group_members_group_id_idx ON group_members (group_id);
+CREATE INDEX IF NOT EXISTS group_members_user_id_idx ON group_members (user_id);
+
+CREATE INDEX IF NOT EXISTS group_members_group_id_user_id_idx ON group_members (group_id, user_id);
